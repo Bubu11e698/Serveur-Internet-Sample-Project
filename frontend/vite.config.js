3cc118c145f058656bb91357410ad7bedbc766da
Mathis Gauthier <mathisgauthier@hotmail.com>
2026-01-05 15:46:20 -0500

    feat: remove proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,12 +17,6 @@ export default defineConfig({
   },
   server: {
     port: 8080,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      }
-    }
+    host: '0.0.0.0'
   }
 })
